8c42325d0935c92a47320b96a51daf7891c76312
com2us-sangdeok <sangdeok@com2us.com>
2022-06-21 10:06:13 +0900

    add terra module

--- test/terra-module.test.ts
+++ test/terra-module.test.ts
@@ -1,13 +1,10 @@
-// import { TerraModule } from '../src/test-module/terra-module';
-const terra = require("../src/test-module/terra-module").TerraModule('http://localhost:1317', 'localterra');
+import { TerraModule } from '../src/test-module/terra-module';
 
 
-// let terra = new TERRA.TerraModule('http://localhost:1317', 'localterra');
+const terra: TerraModule = new TerraModule('http://localhost:1317', 'localterra');
 const mnemonic = 'test test test high purchase tuition stool faith fine install that you unaware feed domain license impose boss human eager hat rent enjoy dawn';
 
-describe('terra test', function () {
-    // let terra: TerraModule;
-
+describe('Terra test', function () {
     describe('Wallet Create Test', function() {
         it('create wallet', async function() {
             const result = await terra.createWallet(mnemonic);
